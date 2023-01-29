76f9b609fd544feb75f2e53372c3c9f573bb820a
knutinator <jonatanknut@gmail.com>
2023-01-29 07:50:16 +0000

    Add outline for game logic script

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+// Activate game when player clicks an icon //
+
+
+// randomize a CPU choice + display it in game area //
+
+
+// compare player and CPU choices //
+
+
+// Display winner message per round //
+
+
+// count and display the scores for each player //
+
+
+// check if any player has 5 points and display game winner message //
+
+
+// display button to reload game when winner has been decided //
